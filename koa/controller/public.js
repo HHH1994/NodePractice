b89d0b1fddd4e1c84b32d440c504fd4965e33663
HWS <15757135983@163.com>
2018-11-23 00:40:39 +0800

    1. 图片上传模块完善

--- koa/controller/public.js
+++ koa/controller/public.js
@@ -8,7 +8,9 @@ const FileUtil = require("../utils/FileUtil");
 
 const uploadFile = ctx =>{
     console.log( ctx.request.files.file);
-    ctx.body = Result.SuccessResult(1,ctx.request.files.file.path);
+    let filePath = ctx.request.files.file.path.split("/"),
+        len = filePath.length;
+    ctx.body = Result.SuccessResult(1,filePath[len-1]);
 };
 
 
@@ -28,31 +30,14 @@ router.post("file",koabody({
              *  name : formDate对象,文件对应的key值
              *  file : 上传上来的文件对象
              * */
-            //todo:自定义图片名
+
             const ext = FileUtil.getUploadFileExt(file.path);
             // 自定义文件路径
-            file.path = 'D:/Downloads/123.jpg';
+            let customFileName= new Date().valueOf();
+            file.path = 'D:/Downloads/'+customFileName+'.'+ext;
         }
     }
 }),uploadFile);
 
-router.post("file2",koabody({
-    multipart:true, // 支持文件上传
-    encoding:'gzip',
-    formidable: {
-        uploadDir: 'D:/Downloads', // 设置文件上传目录
-        keepExtensions: true,    // 保持文件的后缀
-        maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
-        onFileBegin: (name, file) => { // 文件上传前的设置
-            /*
-             *  name : formDate对象,文件对应的key值
-             *  file : 上传上来的文件对象
-             * */
-            //todo:自定义图片名
-            // 自定义文件路径
-            file.path = 'D:/Downloads/343.jpg';
-        }
-    }
-}),uploadFile);
 
 module.exports = router;
